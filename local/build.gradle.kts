47c241f842b1278a2943b1e58d8d64348424002e
Khesa <vandikalvandi@gmail.com>
2025-01-17 12:48:08 +0700

    initial add local layer

---
+++ local/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
+}
+
+android {
+    namespace = "com.assesment.local"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += mapOf(
+                    "room.schemaLocation" to "$projectDir/schemas",
+                    "room.incremental" to "true"
+                )
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.toVersion(libs.versions.java.get()).toString()
+    }
+}
+
+dependencies {
+    implementation(project.dependencies.platform(libs.core.koin.bom))
+    implementation(libs.bundles.koin)
+    implementation(project(":shared"))
+    implementation(project(":data"))
+    implementation(libs.bundles.caching)
+    ksp(libs.caching.room.compiler)
+}
